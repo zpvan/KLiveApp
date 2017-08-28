dc0901ab139eaa16f14932a8abdd8e79a3dd4904
zpvan <843762968@qq.com>
2017-08-28 15:29:46 +0800

    make LiveFragment & MineFragment
    利用Kotlin Extensions找控件, in MainAcitvity
      import kotlinx.android.synthetic.main.activity_main.*
      import kotlinx.android.synthetic.main.include_bottom.*
    
    通过点击事件简单切换两个Fragment
      LiveFragment & MineFragment

--- app/src/main/java/com/knox/kyingke/activity/MainActivity.kt
+++ app/src/main/java/com/knox/kyingke/activity/MainActivity.kt
@@ -2,12 +2,58 @@ package com.knox.kyingke.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.knox.kyingke.R
+import com.knox.kyingke.fragment.LiveFragment
+import com.knox.kyingke.fragment.MineFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.include_bottom.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        val TAG: String = "MainActivity"
+    }
+
+    val fragmentManager = supportFragmentManager
+
+    override fun onClick(v: View?) {
+        Log.e(TAG, "onClick: ")
+        when (v?.id) {
+            R.id.iv_live -> showLive()
+            R.id.iv_mine -> showMine()
+        }
+    }
+
+    private fun showMine() {
+        /*开始fragment事务*/
+        val trans = fragmentManager.beginTransaction()
+        /*给FrameLayout设置MineFragment*/
+        trans.replace(R.id.fl_main, MineFragment())
+        /*提交事务*/
+        trans.commit()
+    }
+
+    private fun showLive() {
+        /*开始fragment事务*/
+        val trans = fragmentManager.beginTransaction()
+        /*给FrameLayout设置LiveFragment*/
+        trans.replace(R.id.fl_main, LiveFragment())
+        /*提交事务*/
+        trans.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListener()
     }
+
+    private fun initListener() {
+        iv_live.setOnClickListener(this)
+        iv_mine.setOnClickListener(this)
+    }
+
+
 }
