737aa5613281a475f8f1f97c1d847d7b0be0a22c
zpvan <843762968@qq.com>
2017-08-30 10:13:31 +0800

    Configure Fresco using Cache
    给图片配置缓存, 这样更高效更省流量, 只是多占一些空间

--- app/src/main/java/com/knox/kyingke/KInKeApplication.kt
+++ app/src/main/java/com/knox/kyingke/KInKeApplication.kt
@@ -2,7 +2,9 @@ package com.knox.kyingke
 
 import android.app.Application
 import android.content.Context
+import com.facebook.cache.disk.DiskCacheConfig
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.core.ImagePipelineConfig
 
 /**
  * @author Knox.Tsang
@@ -23,6 +25,16 @@ class KInKeApplication : Application() {
         super.onCreate()
         mContext = this
 
-        Fresco.initialize(this)
+        val diskCacheConfig = DiskCacheConfig.newBuilder(this)
+                .setMaxCacheSize((50 * 1024 * 1024).toLong())
+                .setBaseDirectoryPath(externalCacheDir) //如果是SD卡路径，要注意权限
+                .build()
+
+        val config = ImagePipelineConfig.newBuilder(this)
+                .setDownsampleEnabled(true)
+                .setMainDiskCacheConfig(diskCacheConfig)
+                .build()
+
+        Fresco.initialize(this, config)
     }
 }
