4aa8b3aae146aa75dc092ef1bf514c5ec0650d88
zpvan <843762968@qq.com>
2017-09-03 13:54:21 +0800

    do GiftShopFragment, part 2
    制作指示器, 并与ViewPager关联

--- app/src/main/java/com/knox/kyingke/fragment/liveroom/GiftShopFragment.kt
+++ app/src/main/java/com/knox/kyingke/fragment/liveroom/GiftShopFragment.kt
@@ -2,12 +2,15 @@ package com.knox.kyingke.fragment.liveroom
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.knox.kyingke.R
 import com.knox.kyingke.adapter.liveroom.GiftRvAdapter
 import com.knox.kyingke.adapter.liveroom.GiftShopVpAdapter
@@ -15,6 +18,7 @@ import com.knox.kyingke.bean.liveroom.GiftBean
 import com.knox.kyingke.bean.liveroom.GiftListBean
 import com.knox.kyingke.http.IRetrofitConnect
 import com.knox.kyingke.http.KRetrofic
+import com.knox.kyingke.utils.KSimpleUtil
 import kotlinx.android.synthetic.main.frag_gift_shop.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -46,13 +50,58 @@ class GiftShopFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        initListener()
         initGiftShopVp()
         requstGifts()
+    }
+
+    private fun initListener() {
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                selectIndicator(position)
+            }
+
+        })
+    }
+
+    private fun selectIndicator(position: Int) {
+        val count = ll_dots.childCount
+        var index = 0
+        while (index < count) {
+            if (index == position) {
+                /*选中的*/
+                ll_dots.getChildAt(index).setBackgroundResource(R.drawable.white_dot)
+            } else {
+                /*非选中的*/
+                ll_dots.getChildAt(index).setBackgroundResource(R.drawable.gray_dot)
+            }
+            index++
+        }
     }
 
     private fun initGiftShopVp() {
         giftShopVpAdapter = GiftShopVpAdapter()
         viewPager.adapter = giftShopVpAdapter
+    }
+
+    private fun initIndicator(count: Int) {
+        var params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        params.setMargins(0, 0, KSimpleUtil.Kdp2px(5f), 0)
+        var index = 0
+        while (index < count) {
+            val dot = ImageView(context)
+            dot.setBackgroundResource(R.drawable.gray_dot)
+            ll_dots.addView(dot, params)
+            index++
+        }
     }
 
     private fun requstGifts() {
@@ -73,6 +122,10 @@ class GiftShopFragment : Fragment() {
     private fun handleGiftsBean(giftBeans: MutableList<GiftBean>?) {
         /*计算gifts需要多少页才能展示完*/
         var pageNum = (giftBeans?.size!! + itemPerPage - 1) / itemPerPage
+
+        /*制作指示器, 需要知道有多少页才知道制作多少个小点点*/
+        initIndicator(pageNum)
+
         /*制作recyclerView集合*/
         val giftRvs: MutableList<RecyclerView> = mutableListOf()
         var rIndex = 0
@@ -98,6 +151,9 @@ class GiftShopFragment : Fragment() {
             rIndex++
         }
         giftShopVpAdapter?.setDatas(giftRvs)
+
+        /*指示器默认选中当前选择的item*/
+        selectIndicator(viewPager.currentItem)
     }
 
     fun switchContent() {
